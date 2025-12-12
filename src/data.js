4e5ed744eef35fd0f1795783e78c11cd310fdadf
Intannurul <nurulintan7744@gmail.com>
2025-12-12 22:14:00 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,167 @@
+import HeroImage from "/assets/hero-img2.png";
+import HeroImage2 from "/assets/hero-img.png";
+
+import CvFile from "/assets/intancv.pdf";
+
+const Image = {
+  HeroImage,
+  CvFile,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/wordpress.webp";
+import Tools3 from "/assets/tools/reactjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+// import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/corel.webp";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "Wordpress",
+    ket: "CMS / Platform",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  // {
+  //   id: 7,
+  //   gambar: Tools7,
+  //   nama: "Node JS",
+  //   ket: "Javascript Runtime",
+  //   dad: "700",
+  // },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "Corel Draw",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.png";
+import Proyek2 from "/assets/proyek/proyek2.png";
+import Proyek3 from "/assets/proyek/proyek3.png";
+import Proyek4 from "/assets/proyek/proyek4.png";
+import Proyek5 from "/assets/proyek/proyek5.png";
+import Proyek6 from "/assets/proyek/proyek6.png";
+
+export const listProyek = [
+  {
+    id: 1,
+    gambar: Proyek1,
+    nama: "Website Keripik Pisang",
+    desk: "Proyek web pertama saya, menampilkan landing page produk makanan UMKM dengan desain yang bersih, sederhana dan Sudah dilengkapi dengan integrasi direct chat WhatsApp yang fungsional, memprioritaskan komunikasi instan dan kemudahan konversi untuk UMKM.",
+    tools: ["HTML", "CSS", "Javascript"],
+    dad: "200",
+    link: "https://intannurul.pelatihanwebdev.com/keripikpisang/",
+  },
+  {
+    id: 2,
+    gambar: Proyek2,
+    nama: "Company Profile",
+    desk: "Website Company Profile berfungsi sebagai etalase digital yang profesional. Dilengkapi dengan fitur animasi halus di Hero Section dan integrasi direct chat WhatsApp untuk komunikasi bisnis dan konversi pelanggan yang efisien.",
+    tools: ["HTML", "CSS", "Javascript"],
+    dad: "300",
+    link: "https://intannurul.pelatihanwebdev.com/kompro/",
+  },
+  {
+    id: 3,
+    gambar: Proyek3,
+    nama: "Web Sereno Pillow",
+    desk: "Proyek pertama saya dalam pembelajaran WordPress, menghasilkan website Company Profile fungsional tanpa coding berat, memanfaatkan tema dan page builder.",
+    tools: ["Wordpress"],
+    dad: "400",
+    link: "https://intannurul.pelatihanwebdev.com/home-sereno/",
+  },
+  {
+    id: 4,
+    gambar: Proyek4,
+    nama: "Web Balai Latihan Kerja",
+    desk: "Website Balai Latihan Kerja (BLK) Kota Tangerang, dibangun menggunakan WordPress untuk memfasilitasi publikasi layanan pemerintah, Menghadirkan fitur utama seperti Info Pelatihan, Lowongan Kerja, dan Berita/Event yang dinamis.",
+    tools: ["Wordpress"],
+    dad: "500",
+    link: "https://intannurul.pelatihanwebdev.com/BLK/",
+  },
+  {
+    id: 5,
+    gambar: Proyek5,
+    nama: "Web Mochi",
+    desk: "Website e-commerce  sederhana untuk produk Mochi, dibuat dengan WordPress, menampilkan desain visual yang segar dan menarik. Dilengkapi fitur integrasi Email fungsional  untuk memudahkan pemesanan pelanggan.",
+    tools: ["Wordpress", "HTML", "CSS", "Javascript"],
+    dad: "600",
+    link: "https://intannurul.pelatihanwebdev.com/Mochi/",
+  },
+  {
+    id: 6,
+    gambar: Proyek6,
+    nama: "Website Portofolio",
+    desk: "Website Portofolio Pribadi  yang sepenuhnya responsif, dirancang untuk menampilkan skill dan proyek pengembangan web saya secara profesional. Dilengkapi fitur penting Download CV instan, memastikan calon perekrut dapat dengan mudah mengakses dokumen kualifikasi saya.!",
+    tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
+    dad: "700",
+    link: "https://intannurul.github.io/portofolio/",
+  },
+];
