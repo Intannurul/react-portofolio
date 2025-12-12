521dd1d81f36970e3120b6a3af1fbdd4c0420b48
Intannurul <nurulintan7744@gmail.com>
2025-12-13 01:37:17 +0700

    update terbaru

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // ← ini penting!
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // ← ini juga penting!
-  ],
+  plugins: [react(), tailwindcss()],
 });
